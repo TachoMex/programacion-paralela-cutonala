53af36609d8212ff66850159ce5f49914928d829
Gilberto Vargas <tachoguitar@gmail.com>
2017-09-02 13:07:25 -0500

    [P-1] Add testing templating

---
+++ practica_1/practica.rb
@@ -0,0 +1,8 @@
+def ceasars_crypt(str)
+end
+
+def palindrome?(str)
+end
+
+def sum_squares(l)
+end
---
+++ practica_1/test.rb
@@ -0,0 +1,13 @@
+require_relative 'practica'
+
+puts ceasars_crypt('hola Mundo233___')
+puts ceasars_crypt('UBYNZHAQB')
+puts ceasars_crypt('abcd')
+puts ceasars_crypt('NOPQ')
+
+puts palindrome?('abcdcba')
+puts palindrome?('abcddcba')
+puts palindrome?('nope')
+
+puts sum_squares([1, 2, 3, 4])
+puts sum_squares((1..10))
